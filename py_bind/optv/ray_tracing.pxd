d224f2332a10bfa710fac1fccd70d9a5c07067cf
acorbe <corbisoft.codes@gmail.com>
2013-10-29 17:38:32 +0100

    ray tracing binding based on cdef class. Input to the class is given via file names which are forwarded to C functions. C structs containing the parameters are bound in the class.

--- py_bind/optv/ray_tracing.pxd
+++ py_bind/optv/ray_tracing.pxd
@@ -13,16 +13,38 @@ cdef extern from "optv/calibration.h":
     ctypedef struct Glass:
         double vec_x,vec_y,vec_z
 
+    ctypedef struct ap_52:
+        double k1,k2,k3,p1,p2,scx,she
 
+    ctypedef struct Calibration:
+        Exterior ext_par
+        Interior int_par
+        Glass glass_par
+        ap_52 added_par
 
-cdef extern from "optv/parameters.h":
+    
+
+cdef extern from "optv/parameters.h":   
+
     ctypedef struct mm_np:
         int  	nlay 
         double  n1
-        double  n2[3]
-        double  d[3]
+        double  *n2 #[3]
+        double  *d #[3]
         double  n3
         int     lut
+
+    ctypedef struct control_par:
+        mm_np *mm
+
+cdef class Ray_tracing:
+    cdef:        
+        Calibration par_calibration
+        int _owns_par_control_par
+        control_par* par_control_par 
+
+        
+
 
 
 
--- py_bind/optv/ray_tracing.pyx
+++ py_bind/optv/ray_tracing.pyx
@@ -1,8 +1,21 @@
+from cython cimport view
 from cython.view cimport array as cvarray
+
+from libc.stdlib cimport malloc #, free
+
 import numpy as np
 
 cdef extern from "optv/ray_tracing.h":
     void c_ray_tracing_v2  "ray_tracing_v2" (double x , double y , Exterior Ex , Interior I , Glass G , mm_np mm , double *Xb2 , double *Yb2 , double *Zb2 , double *a3 , double *b3 , double *c3)
+
+
+cdef extern from "optv/calibration.h":
+    int c_read_ori "read_ori" (Exterior *Ex, Interior *I, Glass *G, char *ori_file, ap_52 *addp, char *add_file, char *add_fallback)
+
+cdef extern from "optv/parameters.h":
+    control_par* c_read_control_par "read_control_par"(char *filename)
+    void c_free_control_par "free_control_par"(control_par *cp)
+
 
 
 
@@ -21,6 +34,40 @@ class pmm_np:
         self.n3 = 0
         self.lut = 0
 
+def get_dummy_mm_np():
+    ret = pmm_np()
+    ret.nlay = 3
+    ret.n1 = 1.
+    ret.n2 = np.array([1.49,0.,0])
+    ret.d = np.array([5.,0.,0])
+    ret.n3 = 1.33
+    ret.lut = 1
+
+    return ret
+
+#cdef mm_np get_dummy_mm_np():
+# def get_dummy_mm_np():
+#     cdef:
+#         mm_np ret
+#         view.array n2view = <double[:3]> ret.n2 
+#         view.array dview = <double[:3]> ret.d 
+
+#     ret.nlay = 3
+#     ret.n1 = 1.
+#     n2view = np.array([1.49,0.,0])
+#     dview = np.array([5.,0.,0])
+#     # for i in range(3):
+#     #     ret.n2[i] = n2view[i]
+#     #     ret.d[i] = dview[i]
+#     ret.n3 = 1.33
+#     ret.lut = 1
+
+#     return ret
+
+    
+
+
+
 def get_dummy_Interior():
     return {'xh':0. , 'yh':0. , 'cc':100.}
 
@@ -38,18 +85,25 @@ def get_dummy_Exterior():
 
     return ret
 
-def get_dummy_mm_np():
-    ret = pmm_np()
-    ret.nlay = 3
-    ret.n1 = 1.
-    ret.n2 = np.array([1.49,0.,0])
-    ret.d = np.array([5.,0.,0])
-    ret.n3 = 1.33
-    ret.lut = 1
-
-    return ret
 
         
+# def ray_tracing(x, Ex, Interior I_, Glass G_,mm_np mm_):
+#     cdef:
+#        double x_, y_ , Xb2, Yb2, Zb2 , a3, b3, c3
+#        Exterior Ex_
+#     Ex_.x0,Ex_.y0,Ex_.z0 = Ex.x0,Ex.y0,Ex.z0
+#     Ex_.omega,Ex_.phi,Ex_.kappa = Ex.omega,Ex.phi,Ex.kappa
+#     for i in range(3):
+#         for j in range(3):
+#             Ex_.dm[i][j] = Ex.dm[i][j]
+#     x_ = x[0]
+#     y_ = x[1]   
+#     c_ray_tracing_v2( x_, y_, Ex_, I_, G_, mm_, &Xb2 , &Yb2 , &Zb2 , &a3, &b3 , &c3 )
+#     return (Xb2, Yb2, Zb2), (a3, b3, c3) #ALL OK!
+
+
+
+
 def ray_tracing(x , Ex ,  I , G, mm):
     cdef:
        double x_, y_ , Xb2, Yb2, Zb2 , a3, b3, c3
@@ -82,14 +136,111 @@ def ray_tracing(x , Ex ,  I , G, mm):
 
     c_ray_tracing_v2( x_, y_, Ex_, I_, G_, mm_, &Xb2 , &Yb2 , &Zb2 , &a3, &b3 , &c3 )
 
-
-    
-
-
     return (Xb2, Yb2, Zb2), (a3, b3, c3) #ALL OK!
 
 
+cdef class Ray_tracing:
     
+    def __init__(self):
+        self._owns_par_control_par = 0
+
+    def read_ori(self,ori_file):
+        return c_read_ori( &self.par_calibration.ext_par
+                           , &self.par_calibration.int_par
+                           , &self.par_calibration.glass_par                           
+                           , ori_file
+                           , &self.par_calibration.added_par
+                           , NULL
+                           , NULL)
+
+    def read_control_par(self,filename):
+        cdef:
+            control_par* ret 
+        ret = c_read_control_par(filename)
+        if ret != NULL:            
+            self.par_control_par = ret
+            self._owns_par_control_par = 1
+
+    def trace(self,x):
+        cdef:
+            double x_ ,  y_ , Xb2 , Yb2 , Zb2 , a3, b3, c3
+        
+        x_ = x[0]
+        y_ = x[1]   
+
+        c_ray_tracing_v2( x_, y_
+                          , self.par_calibration.ext_par
+                          , self.par_calibration.int_par
+                          , self.par_calibration.glass_par
+                          , self.par_control_par[0].mm[0]
+                          , &Xb2 , &Yb2 , &Zb2
+                          , &a3, &b3 , &c3 )
+
+        return (Xb2, Yb2, Zb2), (a3, b3, c3) #ALL OK!
+
+
+    def force_set_exterior(self,Ex):
+        self.par_calibration.ext_par.x0 \
+        , self.par_calibration.ext_par.y0 \
+        , self.par_calibration.ext_par.z0 = Ex.x0, Ex.y0, Ex.z0
+
+        self.par_calibration.ext_par.omega \
+        , self.par_calibration.ext_par.phi \
+        , self.par_calibration.ext_par.kappa = Ex.omega,Ex.phi,Ex.kappa
+
+        for i in range(3):
+            for j in range(3):
+                self.par_calibration.ext_par.dm[i][j] = Ex.dm[i][j]
+
+
+    def force_set_interior(self,In):
+        self.par_calibration.int_par = In
+
+    def force_set_glass(self,Gl):
+        self.par_calibration.glass_par = Gl
+
+    def force_set_mm_np(self,mm):
+        self.dealloc_par_control_par()
+        self.par_control_par = <control_par *>malloc(sizeof(control_par))
+        self.par_control_par[0].mm = <mm_np *> malloc(sizeof(mm_np))
+
+        cdef:
+            mm_np * mm_
+
+        mm_ = self.par_control_par[0].mm
+            
+         
+        mm_[0].nlay = mm.nlay
+        mm_[0].n1 = mm.n1
+        for i in range(3):
+            mm_[0].n2[i] = mm.n2[i]
+            mm_[0].d[i] = mm.d[i]
+        
+        mm_[0].n3 = mm.n3
+        mm_[0].lut = mm.lut 
+
+    def __dealloc__(self):
+        self.dealloc_par_control_par()
+
+    def dealloc_par_control_par(self):        
+        if self._owns_par_control_par == 1:
+            c_free_control_par(self.par_control_par)
+            self._owns_par_control_par = 0
+        
+        
+
+        
+        
+
+    #def set_parameters(exterior = None):
+    #    pass
+
+
+
+
+    
+    
+        
        
     
 
