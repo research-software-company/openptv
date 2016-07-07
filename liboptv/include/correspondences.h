9b13d1bde13f1f3f452a4872d5857da02ea3ca03
Alex Liberzon <alex.liberzon@gmail.com>
2016-07-07 21:53:11 +0300

    copied manually from d810a88458f64cb39f55a7dba2172918d9be7f47

---
+++ liboptv/include/correspondences.h
@@ -0,0 +1,48 @@
+/* Utilities for handling sortgrid */
+
+#ifndef CORRESPONDENCES_H
+#define CORRESPONDENCES_H
+
+#include "calibration.h"
+#include "parameters.h"
+#include "tracking_frame_buf.h"
+#include "imgcoord.h"
+#include "multimed.h"   
+#include "trafo.h"
+#include "vec_utils.h"
+#include "sortgrid.h"
+#include "epi.h"
+#include "btree.h"
+#include <stdio.h>
+
+#define maxcand 200 
+#define nmax 20240
+
+typedef struct
+{
+  int    	p1;	       	    /* point number of master point */
+  int    	n;	       	    /* # of candidates */
+  int    	p2[maxcand];	/* point numbers of candidates */
+  double	corr[maxcand];	/* feature based correlation coefficient */
+  double	dist[maxcand];	/* distance perpendicular to epipolar line */
+}
+correspond;	       	        /* correspondence candidates */
+
+typedef struct
+{
+  int     p[4];             /* 4 is probably the number of cameras, to be fixed to N */
+  double  corr;
+}
+n_tupel;
+
+
+int advance_nd_iterator(int nd, int *current, int *count);
+n_tupel **correspondences(frame *frm, Calibration **calib, volume_par *vpar, 
+    control_par *cpar);
+
+void quicksort_coord2d_x (coord_2d	*crd, int num);
+void qs_coord2d_x (coord_2d	*crd, int left, int right);
+
+
+#endif
+
