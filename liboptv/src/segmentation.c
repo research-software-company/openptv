7ab3134a418d565101ecb002e5c760cee8aba2e0
Alex Liberzon <alex.liberzon@gmail.com>
2016-10-02 16:02:16 +0300

    obviously unnecessary assignment as xn,yn are not used anymore.
    it was here for display purposes, xn = int(x);

--- liboptv/src/segmentation.c
+++ liboptv/src/segmentation.c
@@ -93,7 +93,6 @@ int targ_rec (unsigned char *img, target_par *targ_par, int xmin,
       /* => local maximum, 'peak' */
           {
             yn=i;  xn=j;
-            xn = xn;
             sumg = gv;  *(img0 + i*imx + j) = 0;
             xa = xn;  xb = xn;  ya = yn;  yb = yn;
             gv -= thres;
@@ -188,10 +187,7 @@ int targ_rec (unsigned char *img, target_par *targ_par, int xmin,
                     pix[n_targets].tnr = CORRES_NONE;
                     pix[n_targets].pnr = n_targets;
                     n_targets++;
-                    
-                    xn = x;  
-                    yn = y;
-              }
+             }
           } /*  end of if-loop  */
     }
     free(img0);
